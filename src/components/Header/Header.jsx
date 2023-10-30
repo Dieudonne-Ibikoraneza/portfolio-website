7b7242cc2cd532fdedf797d236d60ee0b1b189b0
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2023-10-30 11:00:23 +0200

    Finished Logo and Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
           {/* ===============  logo  ============ */}
           <div className="flex items-center gap-[10px]">
             <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
-              M
+              D
             </span>
             <div className="leading-[20px]">
               <h2 className="text-xl text-smallTextColor font-[700]">Diox</h2>
@@ -18,11 +18,11 @@ const Header = () => {
           {/* ===============  logo end ============ */}
           {/* ===============  menu start  ============ */}
           <div className="menu">
-            <ul>
-              <li><a href="#about"></a>About</li>
-              <li><a href="#services"></a>Services</li>
-              <li><a href="#portfolio"></a>Contact</li>
-              <li><a href="#contact">Contact</a></li>
+            <ul className="flex items-center gap-10">
+              <li><a href="#about" className="text-smallTextColor font-[600]"></a>About</li>
+              <li><a href="#services" className="text-smallTextColor font-[600]"></a>Services</li>
+              <li><a href="#portfolio" className="text-smallTextColor font-[600]"></a>Contact</li>
+              <li><a href="#contact" className="text-smallTextColor font-[600]">Contact</a></li>
             </ul>
           </div>
           {/* ===============  menu end  ============ */}
