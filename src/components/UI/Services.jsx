0935251a48918b24fd5655b4bf675bf9206c041e
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2023-11-05 04:13:21 +0200

    Finished the Hero cards: Part 3

--- src/components/UI/Services.jsx
+++ src/components/UI/Services.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import frontendImg from "../../assets/images/front-end.png";
+import backendImg from "../../assets/images/backend.png";
+import uiImg from "../../assets/images/design.png";
+import appImg from "../../assets/images/apps.png";
 
 const Services = () => {
   return (
@@ -22,7 +26,7 @@ const Services = () => {
               {/* ============= vertical line running through the middle ========== */}
               <div className="hidden absolute w-1 sm:block bg-indigo-300 h-full left-1/2 transform -translate-x-1/2"></div>
 
-              {/* ========== left card ============ */}
+              {/* ========== left card 1 ============ */}
               <div className="mt-6 sm:mt-0 sm:mb-12">
                 <div className="flex items-center flex-col sm:flex-row">
                   <div className="flex justify-start w-full mx-auto items-center">
@@ -32,15 +36,103 @@ const Services = () => {
                         data-aos-duration="1200"
                         className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
                       >
-                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-2xl">
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
                           Frontend Development
                         </h3>
-                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500]">
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
                           Lorem ipsum, dolor sit amet consectetur adipisicing
                           elit. Optio enim aliquid ratione velit quaerat beatae.
                         </p>
                       </div>
                     </div>
+                  </div>
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 trnsform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={frontendImg} alt="" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              {/* ========== right card 1 ============ */}
+              <div className="mt-6 sm:mt-0 sm:mb-12">
+                <div className="flex items-center flex-col sm:flex-row">
+                  <div className="flex justify-end w-full mx-auto items-center">
+                    <div className="w-full sm:w-1/2 sm:pl-8">
+                      <div
+                        data-aos="fade-left"
+                        data-aos-delay="100"
+                        data-aos-duration="1300"
+                        className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
+                      >
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
+                          Backend Development
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
+                          Lorem ipsum, dolor sit amet consectetur adipisicing
+                          elit. Optio enim aliquid ratione velit quaerat beatae.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 trnsform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={backendImg} alt="" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              {/* ========== left card 2 ============ */}
+              <div className="mt-6 sm:mt-0 sm:mb-12">
+                <div className="flex items-center flex-col sm:flex-row">
+                  <div className="flex justify-start w-full mx-auto items-center">
+                    <div className="w-full sm:w-1/2 sm:pr-8">
+                      <div
+                        data-aos="fade-right"
+                        data-aos-duration="1200"
+                        className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
+                      >
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
+                          UI/UX Design
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
+                          Lorem ipsum, dolor sit amet consectetur adipisicing
+                          elit. Optio enim aliquid ratione velit quaerat beatae.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 trnsform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={uiImg} alt="" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              {/* ========== right card 2 ============ */}
+              <div className="mt-6 sm:mt-0 sm:mb-12">
+                <div className="flex items-center flex-col sm:flex-row">
+                  <div className="flex justify-end w-full mx-auto items-center">
+                    <div className="w-full sm:w-1/2 sm:pl-8">
+                      <div
+                        data-aos="fade-left"
+                        data-aos-delay="100"
+                        data-aos-duration="1300"
+                        className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150"
+                      >
+                        <h3 className="text-primaryColor font-[700] mb-3 group-hover:text-white group-hover:font-[600] text-xl">
+                          Apps Development
+                        </h3>
+                        <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
+                          Lorem ipsum, dolor sit amet consectetur adipisicing
+                          elit. Optio enim aliquid ratione velit quaerat beatae.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 trnsform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={appImg} alt="" />
+                    </figure>
                   </div>
                 </div>
               </div>
