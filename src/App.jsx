7fc2a74f8a1ec0799a3d77eef5ff9535729bacc6
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-04-23 15:06:49 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return <h1 className="text-indigo-600 text-2xl font-[500]">Hello World</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#8873ef",
+        headingColor: "#081e21",
+        smallTextColor: "#193256",
+      },
+    },
+  },
+  plugins: [],
+};
